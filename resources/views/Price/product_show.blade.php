fd9e3877b86b903bb283d34e9da51fa265d24867
vagrant <vagrant@homestead>
2021-09-01 14:38:48 +0000

    product show

--- resources/views/Price/product_show.blade.php
+++ resources/views/Price/product_show.blade.php
@@ -8,19 +8,110 @@
         <div class="row">
 
             <!-- RIGHT -->
-            <div class="col-lg-9 col-md-9 order-md-2 order-lg-2">
+            <div class="col-lg-9 col-md-9 col-sm-9 order-md-2 order-lg-2">
+
+                <div class="row">
+
+                    <!-- IMAGE -->
+                    <div class="col-lg-6 col-sm-6">
+
+                        <div class="thumbnail relative mb-3">
+
+                            <!--
+                                IMAGE ZOOM
+
+                                data-mode="mouseover|grab|click|toggle"
+                            -->
+                            <figure id="zoom-primary" class="zoom" data-mode="mouseover">
+                                <!--
+                                    zoom buttton
+
+                                    positions available:
+                                        .bottom-right
+                                        .bottom-left
+                                        .top-right
+                                        .top-left
+                                -->
+
+
+                                <!--
+                                    image
+
+                                    Extra: add .image-bw class to force black and white!
+                                -->
+                                <img class="img-fluid" src="{{ asset('') }}storage/{{$product->link}}" width="1200" height="1500" alt="This is the product title" />
+                            </figure>
+
+                        </div>
+
+
+
+                    </div>
+                    <!-- /IMAGE -->
+
+                    <!-- ITEM DESC -->
+                    <div class="col-lg-6 col-sm-6">
+
+                        <!-- buttons -->
+
+                        <!-- /buttons -->
+
+                        <!-- price -->
+                        <div class="shop-item-price">
+                            Цена: {{$product->price}}
+                        </div>
+                        <!-- /price -->
+
+                        <hr />
+
+                        <div class="clearfix mb-30">
+                            <span class="float-right text-success"><i class="fa fa-check"></i> Статус наличия</span>
+                            <!--
+                            <span class="float-right text-danger"><i class="fa fa-remove"></i> Out of Stock</span>
+                            -->
+
+
+                        </div>
+
+
+                        <!-- short description -->
+                        <p>{{$product->Text}}</p>
+                        <!-- /short description -->
+
+
+                        <!-- countdown -->
+                        <div class="text-center">
+                            <h5>Производитель</h5>
+
+                        </div>
+                        <!-- /countdown -->
+
+
+                        <hr />
+
+
+                        <hr />
+
+                        <small class="text-muted">
+                            Возможная информация
+                        </small>
+
+                        <hr />
+
+
+                    </div>
+                    <!-- /ITEM DESC -->
+
+                </div>
 
 
 
 
+                <hr class="mt-80 mb-80" />
 
-
-
-                <!-- Pagination Default -->
-
-                <!-- /Pagination Default -->
 
             </div>
+
 
 
             <!-- LEFT -->
